e895a186a97c1a54fb7d066f0740939796c1f3ac
DastanSE <dastan.zhanibek@gmail.com>
2018-04-08 21:26:15 +0800

    start write test

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -6,7 +6,16 @@ const buildPath = path.resolve(__dirname, 'build');
 
 fs.removeSync(buildPath);
 
-const campaignPath = parh.resolve(__dirname, 'contracts', 'Campaign.sol');
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
 
 const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath);
+ 
+for(let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(':', '') + '.json'),
+    output[contract]
+  );
+}
