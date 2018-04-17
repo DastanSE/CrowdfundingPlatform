af412f6597521926ba59ec8c58c3e26b8b4d8d39
DastanSE <dastan.zhanibek@gmail.com>
2018-04-17 18:25:26 +0800

    add getSummary and getRequest functions to the contract and redeploy contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x5f14B328e7eF8f60D9DA4301B5b513ADbc38d0cc'
+  '0x8d6600fe8Be473f61Ab534Ecd40F300138eF8e1A'
 );
 
 export default instance;
